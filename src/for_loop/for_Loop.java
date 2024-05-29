d8008248c333ffe56d811aa258b23c1c0305fb47
Veeranji49 <veeranjijlkr49@gmail.com>
2024-05-29 15:10:30 +0530

    first commit

---
+++ src/for_loop/for_Loop.java
@@ -0,0 +1,23 @@
+package for_loop;
+
+public class for_Loop {
+    public static void main(String[] args) {
+
+        // The syntax of for loop :
+       /*
+            for (initialExpression; condition; increment/decrement) {
+                 // body of the loop
+            }
+        */
+
+        // To print hello world four times
+        for(int i=1; i<=4; i+=1){
+            System.out.println("Hello World");
+        }
+
+        //To Print 1-5 numbers
+        for(int j=1; j<=5; j+=1){
+            System.out.println(j);
+        }
+    }
+}
