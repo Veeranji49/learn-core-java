b8437c171f8f8911fdc060a126f174ca6618fa7d
Veeranji49 <veeranjijlkr49@gmail.com>
2024-05-30 16:46:07 +0530

    assignment operators is added

---
+++ src/operators/Assignment_Operators.java
@@ -0,0 +1,46 @@
+package operators;
+
+public class Assignment_Operators {
+
+    public static void main(String[] args) {
+
+        // These operators are used to assign values to variables.
+
+
+        // Assignment (=)
+        int num = 5; // num is 5
+        System.out.println(num);
+
+
+        // Addition assignment (+=)
+        int num1 = 5;
+        num1 += 3; // num is 8
+        System.out.println(num1);
+
+
+        // Subtraction assignment (-=)
+        int num2 = 5;
+        num2 -= 3; // num is 2
+        System.out.println(num2);
+
+
+        // Multiplication assignment (*=)
+        int num3 = 5;
+        num3 *= 3; // num is 15
+        System.out.println(num3);
+
+
+        // Division assignment (/=)
+        int num4 = 5;
+        num4 /= 3; // num is 1
+        System.out.println(num4);
+
+
+        // Modulus assignment (%=)
+        int num5 = 5;
+        num5 %= 3; // num is 2
+        System.out.println(num5);
+
+
+    }
+}
