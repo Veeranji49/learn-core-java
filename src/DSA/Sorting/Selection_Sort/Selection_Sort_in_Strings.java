6467975221bd687d00a4f915771e796a5776691d
Veeranji49 <veeranjijlkr49@gmail.com>
2024-06-05 13:54:23 +0530

    selection sort algorithm practiced

---
+++ src/DSA/Sorting/Selection_Sort/Selection_Sort_in_Strings.java
@@ -0,0 +1,39 @@
+package DSA.Sorting.Selection_Sort;
+
+public class Selection_Sort_in_Strings {
+
+    public static void main(String[] args) {
+        String[] array = {"banana", "apple", "orange", "grape", "kiwi"};
+
+        System.out.println("Original array:");
+        printArray(array);
+
+        selectionSort(array);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+    }
+
+    public static void selectionSort(String[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j].compareTo(array[minIndex]) < 0) {
+                    minIndex = j;
+                }
+            }
+            String temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public static void printArray(String[] array) {
+        for (String str : array) {
+            System.out.print(str + " ");
+        }
+        System.out.println();
+    }
+}
