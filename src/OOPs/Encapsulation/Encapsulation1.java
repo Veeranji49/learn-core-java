7cb84d06174a441d0ef97531769bc21c3d3c9b11
Veeranji49 <veeranjijlkr49@gmail.com>
2024-06-09 12:36:29 +0530

    Encapsulation example added

---
+++ src/OOPs/Encapsulation/Encapsulation1.java
@@ -0,0 +1,90 @@
+package OOPs.Encapsulation;
+
+public class Encapsulation1 {
+
+    public static void main(String[] args) {
+
+        Student std = new Student();
+        std.setId(1);
+        std.setName("anji");
+        std.setFees(34232.00);
+        std.setContact(630291781);
+        std.setIshealthy(true);
+        std.setLocation("Peddakadabur");
+
+        int id = std.getId();
+        String name = std.getName();
+        double fees = std.getFees();
+        boolean ishealthy = std.getIsHealthy();
+        long contact = std.getContact();
+        String location = std.getLocation();
+
+        System.out.println("Id :" + id);
+        System.out.println("Name :" + name);
+        System.out.println("Fees :" + fees);
+        System.out.println("IsHealthy :" + ishealthy);
+        System.out.println("Contact :" + contact);
+        System.out.println("Location :" + location);
+    }
+}
+
+class Student {
+
+    private int id;
+    private String name;
+    private double fees;
+    private boolean ishealthy;
+    private long contact;
+    private String location;
+
+
+    public int getId() {
+        return this.id;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getFees() {
+        return this.fees;
+    }
+
+    public void setFees(double fees) {
+        this.fees = fees;
+    }
+
+    public boolean getIsHealthy() {
+        return this.ishealthy;
+    }
+
+    public void setIshealthy(boolean ishealthy) {
+        this.ishealthy = ishealthy;
+    }
+
+    public long getContact() {
+        return this.contact;
+    }
+
+    public void setContact(long contact) {
+        this.contact = contact;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
